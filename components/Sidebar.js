6b555a9531d0dd66a8e2dfc67f958800fd429b8e
Pira Ravikumaran <Pira.Ravikumaran@adidevelopments.com>
2022-10-14 16:12:44 -0400

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { HomeIcon } from '@heroicons/react/24/solid'
+
+function Sidebar() {
+  return (
+    <div>
+        <div>
+        <button>
+            <HomeIcon className="h-5 w-5"/>
+        </button>
+                
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
