e973c50f273edf1c79409f5d3a49079f4d9c07eb
Pira Ravikumaran <pira.ravikumaran@adidevelopments.com>
2022-10-25 01:17:01 -0400

    first working draft

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Center from '../components/Center'
+import Player from '../components/Player'
 import Sidebar from '../components/Sidebar'
 import styles from '../styles/Home.module.css'
 
@@ -10,11 +11,10 @@ export default function Home() {
       <main className='flex'>
         <Sidebar />
         <Center />
-        {/* {Center} */}
       </main>
 
-      <div>
-        {/* {Player} */}
+      <div className='sticky bottom-0'>
+        <Player />
       </div>
     </div>
   )
